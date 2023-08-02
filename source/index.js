6520684ece6b4a4650e8f4d896a9d937e07aa420
Calise425 <caitlin.norris1998@gmail.com>
2023-08-02 17:52:59 -0600

    initial commit

---
+++ source/index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+const App = () => {
+  
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
