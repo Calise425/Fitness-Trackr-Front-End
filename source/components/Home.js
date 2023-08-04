9665ba78998c070edd545854ac351f69dd01ccaf
Calise425 <caitlin.norris1998@gmail.com>
2023-08-03 18:36:54 -0600

    stuff

--- source/components/Home.js
+++ source/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {fetchPublicRoutines} from "../api/apiCalls";
+import {fetchPublicRoutines} from "../api/helperCalls";
 
 const Home = () => {
   const [routines, setRoutines] = useState([]);
