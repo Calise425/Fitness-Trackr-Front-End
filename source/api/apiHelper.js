3dd7eb21f2e6d41208d984bce15d8e7a8a3b10d6
Cameron McEachin <cmceachin5888@gmail.com>
2023-08-06 15:31:54 -0400

    working on apihelpers

--- source/api/apiHelper.js
+++ source/api/apiHelper.js
@@ -1,33 +1,35 @@
-const BASE_URL = "https://fitnesstrac-kr.herokuapp.com/api/2303-ftb-et-web-pt";
+const BASE_URL = "https://fitnesstrac-kr.herokuapp.com/api/";
 
 
 const registerUser = async (
-  name,
-  pass,
-  setToken,
-  setSuccess,
-  setError
-) => {
+  name, 
+  pass, 
+  setToken, 
+  setSuccess, 
+  setError) => {
   try {
-    const response = await fetch(
-      `${BASE_URL}/register`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    const response = await fetch(`${BASE_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: name,
+          password: pass,
         },
-        body: JSON.stringify({
-          user: {
-            username: `${name}`,
-            password: `${pass}`,
-          },
-        }),
-      }
-    );
+      }),
+    });
     const result = await response.json();
-    !result.success ? setError(result.error.message) : null;
-    setToken(result.data.token);
-    result.data.token ? setLoggedIn(true) && setSuccess(true) : null;
+    if (!result.success) {
+      setError(result.error.message);
+    } else {
+      setToken(result.data.token);
+      if (result.data.token) {
+        setLoggedIn(true);
+        setSuccess(true);
+      }
+    }
     return result;
   } catch (err) {
     console.error(err);
@@ -42,20 +44,23 @@ const login = async (username, password, setToken, setSuccess, setError) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-          username: `${username}`,
-          password: `${password}`
+        username: username,
+        password: password
       })
     });
     const result = await response.json();
-    !result.success ? setError(result.error.message) : null;
-    result.data.token ? setLoggedIn(true) && setSuccess(true) : null;
-    setToken(result.data.token);
+    if (!result.success) {
+      setError(result.error.message);
+    } else {
+      setLoggedIn(true);
+      setSuccess(true);
+      setToken(result.data.token);
+    }
     return result;
   } catch (err) {
     console.error(err);
   }
-}
-
+};
 
 const fetchPublicRoutines = async () => {
   try {
@@ -71,6 +76,97 @@ const fetchPublicRoutines = async () => {
   } catch (err) {
   console.error(err);
   }
-}
+};
 
-export {fetchPublicRoutines, login, registerUser}
+const fetchUserData = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+    });
+
+    const result = await response.json();
+
+    return result;
+  } catch (err) {
+    console.error("Couldn't fetch user data", err);
+  }
+};
+
+const fetchActivities = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/activities`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error("No Posts Available", err);
+  }
+};
+
+const makeActivities = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/activities`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        name: name,
+        description: description
+      }) 
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error("Error creating activity", err);
+  }
+};
+
+const updateActivities = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/activities/${activityId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        description: description
+      }) 
+    });
+
+    const result = await response.json();
+    
+  } catch (err) {
+    throw new Error("Failed to update post");
+  }
+  console.log(result);
+  if (!result.success) {
+    throw new Error(result.error.message);
+  }
+  return result.data;
+};
+
+
+export {
+  fetchPublicRoutines, 
+  login, 
+  registerUser, 
+  fetchUserData, 
+  fetchActivities,
+  makeActivities
+};
